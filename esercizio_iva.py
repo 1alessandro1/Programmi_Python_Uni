51b0715892c7795b0569b70ce6077a92f5f9b8db
1alessandro1 <46293832+1alessandro1@users.noreply.github.com>
2022-10-18 16:36:41 +0200

    Aggiunto il ciclo while per la verifica degli input

--- esercizio_iva.py
+++ esercizio_iva.py
@@ -1,11 +1,11 @@
+import sys
 prezzo_con_iva = float(input("Inserire il costo lordo del prodotto: "))
 
 numeratore_valore_iva = int(input("Inserire il valore dell'IVA: "))
 
-if numeratore_valore_iva < 0 or numeratore_valore_iva > 99:
-   print ("Il valore dell'IVA non è accettabile")
-   quit()
-   
+while numeratore_valore_iva < 0 or numeratore_valore_iva > 99:
+  print ("il valore dell'iva non è accettabile, prova ancora")
+  numeratore_valore_iva = int(input("Inserire il valore dell'IVA: "))
 
 valore_netto = (prezzo_con_iva)/(1 + numeratore_valore_iva / 100)
 
