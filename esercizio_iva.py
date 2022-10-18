243e6d51674acf1ab7baa0ddef4158c873ca7d8d
1alessandro1 <46293832+1alessandro1@users.noreply.github.com>
2022-10-18 15:43:50 +0200

    Update esercizio_iva.py

--- esercizio_iva.py
+++ esercizio_iva.py
@@ -2,6 +2,11 @@ prezzo_con_iva = float(input("Inserire il costo lordo del prodotto: "))
 
 numeratore_valore_iva = int(input("Inserire il valore dell'IVA: "))
 
+if numeratore_valore_iva < 0 or numeratore_valore_iva > 99:
+   print ("Il valore dell'IVA non è accettabile")
+   quit()
+   
+
 valore_netto = (prezzo_con_iva)/(1 + numeratore_valore_iva / 100)
 
 print (valore_netto)
