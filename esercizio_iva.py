95f4871a3efe6d62e7f047136435d7a2ba480b91
1alessandro1 <46293832+1alessandro1@users.noreply.github.com>
2022-10-19 01:12:24 +0200

    Piccole correzioni sui valori massimi assumibili

--- esercizio_iva.py
+++ esercizio_iva.py
@@ -3,24 +3,26 @@ def PrendiPrezzoLordoGiusto():
     while True:
         try:
             prezzo_con_iva = float(prezzo_con_iva)
+            print ("Bene, il valore del prezzo lordo che hai inserito è un numero decimale accettabile...")
             break
         except:
-            print("Valore non corretto, riprova")
+            print("Il valore inserito non è composto da soli numeri decimali, riprova...")
             prezzo_con_iva = input("Re-inserisci il prezzo lordo: ")
     return float(prezzo_con_iva)
 
 def PrendiValoreIVAGiusto():
-    numeratore_valore_iva = input("Inserisci il valore del numeratore dell' IVA: ")
+    numeratore_valore_iva = input("Inserisci il valore del numeratore dell'IVA: ")
     while True:
         try:
             numeratore_valore_iva = float(numeratore_valore_iva)
-            if numeratore_valore_iva > 0 and numeratore_valore_iva < 99:
-                print("ok, il tuo valore è compreso nell'intervallo corretto")
+            if numeratore_valore_iva >= 0 and numeratore_valore_iva <= 100:
+                print("Ok, il valore dell'IVA inserito è compreso nell'intervallo corretto...")
+
                 return numeratore_valore_iva 
             else:
-                numeratore_valore_iva = float(input("Re-inserisci un valore compreso tra 0 e 99: "))
+                numeratore_valore_iva = float(input("Reinserisci un valore compreso tra 0 e 100: "))
         except:
-            numeratore_valore_iva = input("Attenzione, hai inserito un valore non numerico: ")
+            numeratore_valore_iva = input("Attenzione, hai inserito un valore che contiene valori non decimali, riprova: ")
 
 
 valore_netto = (PrendiPrezzoLordoGiusto())/(1 + PrendiValoreIVAGiusto() / 100)
