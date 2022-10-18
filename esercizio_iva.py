da400241b9cd6b384cdd68ae0d11f2e0e563f6b1
1alessandro1 <46293832+1alessandro1@users.noreply.github.com>
2022-10-18 16:54:27 +0200

    Tentativo 1 di verifica input
    Se l’utente inserisce qualcosa che non è un numero, sarebbe bello chiedergli di ritentare piuttosto che far incavolare l’interprete per il fatto che il valore inserito non è un float

--- esercizio_iva.py
+++ esercizio_iva.py
@@ -1,11 +1,16 @@
 import sys
 prezzo_con_iva = float(input("Inserire il costo lordo del prodotto: "))
 
+while not numeratore_con_iva.isnumeric():
+  print ("Devi inserire un valore numerico!")
+  
+  
 numeratore_valore_iva = int(input("Inserire il valore dell'IVA: "))
 
 while numeratore_valore_iva < 0 or numeratore_valore_iva > 99:
-  print ("il valore dell'iva non è accettabile, prova ancora")
+  print ("il valore dell'iva non è accettabile")
   numeratore_valore_iva = int(input("Inserire il valore dell'IVA: "))
+
 
 valore_netto = (prezzo_con_iva)/(1 + numeratore_valore_iva / 100)
 
