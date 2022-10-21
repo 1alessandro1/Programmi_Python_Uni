b835591dbd1ca672d8f4646f79bc625e516bf887
1alessandro1 <alessandrozanardi30@gmail.com>
2022-10-21 11:02:26 +0200

    Area e diagonale del rettangolo

--- area-del-rettangolo.py
+++ area-del-rettangolo.py
@@ -1,6 +1,10 @@
+import math
+
 altezza = float(input("Dammi l'altezza: "))
 base = float(input("Dammi la base: "))
 
 Area = base * altezza
 
-print ("L'area del rettangolo vale:", (Area))
+diagonale_del_rettangolo = math.sqrt(altezza*altezza + base*base)
+
+print ("L'area del rettangolor vale:", (Area), "e la sua diagonale vale: ", (diagonale_del_rettangolo))
