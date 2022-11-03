d165388b5368d52280be6c3f477bacfbf09aa0d3
1alessandro1 <alessandrozanardi30@gmail.com>
2022-11-03 11:20:38 +0100

    Aggiunto ciclo while

--- ciclo_while.py
+++ ciclo_while.py
@@ -1,4 +1,15 @@
 x = 0 
 while x<=100:
     print(x, x*x, 2**x)
-    x = x + 5
+    x = x + 1
+ 
+ 
+for c in "Salve!":
+    print(c, ord(c))
+
+s = "Salve!"
+i = 0
+
+while i<=5:
+    print (s[i])
+    i = i + 1
